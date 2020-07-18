d8a1b74d3e03e9e4a346ba8d34af74894e0152ef
Chisato Nakata <peachlily.511@gmail.com>
2020-07-18 11:47:13 +0900

    secondを削除

--- stopwatch/ViewController.swift
+++ stopwatch/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
        
     @IBOutlet weak var timerMinute: UILabel!
     @IBOutlet weak var timerSecond: UILabel!
-    @IBOutlet weak var timerMsec: UILabel!
     
     weak var timer: Timer!
     var startTime = Date()
@@ -24,7 +23,6 @@ class ViewController: UIViewController {
         self.view.backgroundColor = #colorLiteral(red: 0.2128436267, green: 0.646464169, blue: 0.6198984981, alpha: 1)
         timerMinute.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         timerSecond.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        timerMsec.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
    }
 
     @IBAction func startTimer(_ sender: Any) {
@@ -34,7 +32,7 @@ class ViewController: UIViewController {
         }
         
         timer = Timer.scheduledTimer(
-            timeInterval: 0.01,
+            timeInterval: 1,
             target: self,
             selector: #selector(self.timerCounter),
             userInfo: nil,
@@ -50,28 +48,25 @@ class ViewController: UIViewController {
 
             timerMinute.text = "00"
             timerSecond.text = "00"
-            timerMsec.text = "00"
+            
         }
     }
        
     @objc func timerCounter() {
+        
            // タイマー開始からのインターバル時間
            let currentTime = Date().timeIntervalSince(startTime)
            // fmod() 余りを計算
            let minute = (Int)(fmod((currentTime/60), 60))
            // currentTime/60 の余り
            let second = (Int)(fmod(currentTime, 60))
-           // floor 切り捨て、小数点以下を取り出して *100
-           let msec = (Int)((currentTime - floor(currentTime))*100)
            
            // %02d： ２桁表示、0で埋める
            let sMinute = String(format:"%02d", minute)
            let sSecond = String(format:"%02d", second)
-           let sMsec = String(format:"%02d", msec)
            
            timerMinute.text = sMinute
            timerSecond.text = sSecond
-           timerMsec.text = sMsec
         
     }
        
